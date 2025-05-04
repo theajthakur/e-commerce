940643479e95826ebe909bdc89e3e64d78bb29d5
Vijay Singh <thakurvijayofficial@gmail.com>
2025-05-04 11:46:24 +0530

    Navbar Completed

--- src/assets/Cart.jsx
+++ src/assets/Cart.jsx
@@ -2,36 +2,34 @@ import React from "react";
 
 export default function Cart({ cartLength }) {
   return (
-    <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="64"
-        height="64"
-        viewBox="0 0 64 64"
-        fill="none"
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="60"
+      height="60"
+      viewBox="0 0 64 64"
+      fill="none"
+    >
+      <path
+        d="M16 16H20L24 40H48L52 24H28"
+        stroke="#ccc"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <circle cx="26" cy="52" r="3" fill="#ccc" />
+      <circle cx="44" cy="52" r="3" fill="#ccc" />
+      <circle cx="48" cy="12" r="10" fill="#EF4444" />
+      <text
+        x="48"
+        y="16"
+        textAnchor="middle"
+        fontSize="12"
+        fontFamily="Arial, sans-serif"
+        fill="white"
+        fontWeight="bold"
       >
-        <path
-          d="M16 16H20L24 40H48L52 24H28"
-          stroke="#333"
-          strokeWidth="3"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <circle cx="26" cy="52" r="3" fill="#333" />
-        <circle cx="44" cy="52" r="3" fill="#333" />
-        <circle cx="48" cy="12" r="10" fill="#EF4444" />
-        <text
-          x="48"
-          y="16"
-          textAnchor="middle"
-          fontSize="12"
-          fontFamily="Arial, sans-serif"
-          fill="white"
-          fontWeight="bold"
-        >
-          {cartLength}
-        </text>
-      </svg>
-    </div>
+        {cartLength}
+      </text>
+    </svg>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import Logo from "../assets/Logo.svg";
+import Cart from "../assets/Cart";
+import "../style/Navbar.css";
+import { CrossIcon, Search, X } from "lucide-react";
+
+function Navbar({ cart }) {
+  const [searchVisibility, setSearchVisibility] = useState(false);
+  const productNames = [
+    "Leather Jacket",
+    "Denim Jeans",
+    "Floral Maxi Dress",
+    "Bluetooth Headphones",
+    "Memory Foam Pillow",
+    "Running Sneakers",
+    "Cotton Hoodie",
+    "Aviator Sunglasses",
+    "Silk Scarf",
+    "Crossbody Bag",
+  ];
+  const [placeholder, setPlaceholder] = useState(productNames[0]);
+  useState(() => {
+    setInterval(() => {
+      const randomProduct =
+        productNames[Math.floor(Math.random() * productNames.length)];
+      setPlaceholder(randomProduct);
+    }, 3000);
+  }, []);
+  const navOptions = [
+    {
+      component: searchVisibility ? (
+        <input className="search-form-input" placeholder={placeholder} />
+      ) : (
+        ""
+      ),
+      title: "Search Field",
+    },
+    {
+      component: searchVisibility ? (
+        <X
+          color="white"
+          onClick={() => {
+            setSearchVisibility(!searchVisibility);
+          }}
+        />
+      ) : (
+        <Search
+          color="white"
+          onClick={() => {
+            setSearchVisibility(!searchVisibility);
+          }}
+        />
+      ),
+      title: "Search Products",
+    },
+    { component: <Cart cartLength={cart.length} />, title: "Cart Items" },
+  ];
+  return (
+    <nav className="navbar navbar-expand navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          <div className="logo-animate">
+            <img src={Logo} />
+            <span>Treshop</span>
+          </div>
+        </a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto align-items-center navoption-container">
+            {navOptions.map((nv, index) => (
+              <li
+                title={nv.title || ""}
+                className={`nav-item ${
+                  index > 0 ? "ms-lg-5 ms-2 ms-sm-3" : ""
+                }`}
+                key={index}
+              >
+                {nv.component}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
