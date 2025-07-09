220b40b48b02f12bf5fba37cac5a04432bfbd158
Vijay Singh <thakurvijayofficial@gmail.com>
2025-07-09 22:19:27 +0530

    Fixed DOM

--- src/components/utils/Loader.jsx
+++ src/components/utils/Loader.jsx
@@ -4,7 +4,7 @@ import "../../style/Loader.css";
 export default function Loader() {
   return (
     <div className="loader-container">
-      <div class="lds-roller">
+      <div className="lds-roller">
         <div></div>
         <div></div>
         <div></div>
